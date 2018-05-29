0223602ea3ff1f81064aee53d68c5288f24021cd
Rajas Bansal <rajasbansal1998@gmail.com>
2018-05-29 15:46:27 +0800

    added the print log

--- src/main/scala/lsu.scala
+++ src/main/scala/lsu.scala
@@ -1011,7 +1011,9 @@ class LoadStoreUnit(pl_width: Int)(implicit p: Parameters, edge: uncore.tilelink
    // val isValidated = Reg(next = io.exe_resp.valid && dtlb.io.req.valid,init = false)
    io.emit_validated.valid := can_fire_load_wakeup
    io.emit_validated.bits.rob_idx := laq_uop(exe_ld_idx_wakeup).rob_idx
-
+   when (io.emit_validated) {
+      printf("\n---- This load has been validated %d\n", io.emit_validated.bits.rob_idx)
+   }
 
    //-------------------------------------------------------------
    // Kill speculated entries on branch mispredict
