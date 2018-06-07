a078bd50848fd5012943e5c2b33861cadf7083d8
Rajas Bansal <rajasbansal1998@gmail.com>
2018-06-07 16:53:39 +0800

    Fixed io absence

--- src/main/scala/issue_ageordered.scala
+++ src/main/scala/issue_ageordered.scala
@@ -144,8 +144,8 @@ class IssueUnitCollasping(
 
    for (i <- 0 until num_issue_slots)
    {
-      mispredicted_uops(i).valid := issue_slots(i).killed_by_branch
-      mispredicted_uops(i).bits  := issue_slots(i).uop
+      io.mispredicted_uops(i).valid := issue_slots(i).killed_by_branch
+      io.mispredicted_uops(i).bits  := issue_slots(i).uop
    }
 
 }
